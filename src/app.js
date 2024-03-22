6a884c874bffd0fdb05b87dc02f5d6f93c8b2d26
akshaydubey102 <akshaydubey102@gmail.com>
2024-03-23 00:02:14 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import router from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 app.use('/api/wheather', router);
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,4 +11,13 @@ router.get('/data', async (req, res) => {
   return res.json(data1);
 });
 
+router.get('/data/bylatandlon', async (req, res) => {
+  const { lat, lon } = req.query;
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.APIKEY}`
+  );
+  const data1 = await data.json();
+  return res.json(data1);
+});
+
 export default router;
